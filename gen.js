9c0f33402f7cb0918a561e720e672dbe05521a76
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-10-02 11:43:30 +0700

    change event type from mousemove to input. move all variables to top of page

--- gen.js
+++ gen.js
@@ -1,11 +1,22 @@
-const slider = document.querySelector("#age");
-const output = document.querySelector("#age-value");
+const slider = document.querySelector("#age"); /* input */
+const output = document.querySelector("#age-value"); /* output */
+const howOftenSlider = document.querySelector("#how-many-times"); /* input */
+const howOftenOutput = document.querySelector("#how-many-times-output-value"); /* output */
 
-slider.addEventListener("mousemove", function() {
+slider.addEventListener("input", function() {
     let x = slider.value;
-    var color = `linear-gradient(90deg, rgb(115, 207, 223) ${x}%, rgb(214, 214, 214) ${x}%)`;
+    var color = `linear-gradient(90deg, rgb(142, 202, 230) ${x}%, rgb(214, 214, 214) ${x}%)`;
 
     slider.style.background = color;
 });
+
+howOftenSlider.addEventListener("input", function() {
+    let x = howOftenSlider.value;
+    var color = `linear-gradient(90deg, rgb(115, 207, 223) ${x * 14.2}%, rgb(214, 214, 214) ${x * 14.2}%)`;
+
+    howOftenSlider.style.background = color;
+});
+
+
 
 
