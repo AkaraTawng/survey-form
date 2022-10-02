f7669d621307e00122de20ffa4f89909540f8ef0
Christopher Chamberlain <c.r.chamberlain1997@gmail.com>
2022-10-02 09:56:49 +0700

    add slider background color change function

--- gen.js
+++ gen.js
@@ -1,8 +1,11 @@
-function gen(){
-    console.log("pear");
-    console.log("banana");
-    console.log("apple");
-};
+const slider = document.querySelector("#age");
+const output = document.querySelector("#age-value");
 
-gen();
+slider.addEventListener("mousemove", function() {
+    let x = slider.value;
+    var color = `linear-gradient(90deg, rgb(115, 207, 223) ${x}%, rgb(214, 214, 214) ${x}%)`;
+
+    slider.style.background = color;
+})
+
 
